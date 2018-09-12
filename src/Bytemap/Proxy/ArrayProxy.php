76f418e4d39cf4ac6c99d5732fb00c937197d292
Robert Gust-Bardon <id+gpg@robert.gust-bardon.org>
2018-09-12 00:46:12 -0500

    Reclassify as a proxy and introduce an interface

---
+++ src/Bytemap/Proxy/ArrayProxy.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Bytemap package.
+ *
+ * (c) Robert Gust-Bardon <robert@gust-bardon.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bytemap\Proxy;
+
+use Bytemap\Bytemap;
+use Bytemap\BytemapInterface;
+
+class ArrayProxy implements ArrayProxyInterface
+{
+    /** @var BytemapInterface */
+    protected $bytemap;
+
+    public function __construct(string $defaultItem, string ...$values)
+    {
+        $this->bytemap = new Bytemap($defaultItem);
+        $this->bytemap->insert($values);
+    }
+
+    public static function fill(string $defaultItem, int $startIndex, int $num, ?string $value = null): ArrayProxyInterface
+    {
+        if ($startIndex < 0) {
+            throw new \OutOfRangeException('Start index can\'t be negative');
+        }
+
+        if ($num < 0) {
+            throw new \OutOfRangeException('Number of elements can\'t be negative');
+        }
+
+        if (null === $value) {
+            $value = $defaultItem;
+        }
+
+        $arrayProxy = new self($defaultItem);
+        $arrayProxy->bytemap->insert((function () use ($num, $value): \Generator {
+            for ($i = 0; $i < $num; ++$i) {
+                yield $value;
+            }
+        })(), $startIndex);
+
+        return $arrayProxy;
+    }
+}
---
+++ src/Bytemap/Proxy/ArrayProxyInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Bytemap package.
+ *
+ * (c) Robert Gust-Bardon <robert@gust-bardon.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bytemap\Proxy;
+
+interface ArrayProxyInterface
+{
+    /**
+     * `array_fill` (fill an array with values).
+     *
+     * @param string $defaultItem the default item of the underlying bytemap
+     * @param int    $startIndex  the first index of the value used for filling
+     * @param int    $num         number of elements to insert
+     * @param string $value       value to use for filling
+     */
+    public static function fill(string $defaultItem, int $startIndex, int $num, ?string $value = null): self;
+}
