67e403eff83103bf277459233eb9e2452b2e5d18
Robert Gust-Bardon <id+gpg@robert.gust-bardon.org>
2019-01-30 23:56:04 -0500

    Benchmark all the implementations using PhpBench

--- tests/Bytemap/Performance/BytemapPerformance.php
+++ tests/Bytemap/Performance/BytemapPerformance.php
@@ -11,7 +11,14 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Bytemap;
+namespace Bytemap\Performance;
+
+use Bytemap\Bitmap;
+use Bytemap\Bytemap;
+use Bytemap\Benchmark\ArrayBytemap;
+use Bytemap\Benchmark\DsDequeBytemap;
+use Bytemap\Benchmark\DsVectorBytemap;
+use Bytemap\Benchmark\SplBytemap;
 
 /**
  * @author Robert Gust-Bardon <robert@gust-bardon.org>
@@ -22,22 +29,44 @@ namespace Bytemap;
  */
 final class BytemapPerformance
 {
+    private const IMPLEMENTATIONS = [
+        ArrayBytemap::class => "\x1",
+        DsDequeBytemap::class => "\x1",
+        DsVectorBytemap::class => "\x1",
+        SplBytemap::class => "\x1",
+        Bytemap::class => "\x1",
+        Bitmap::class => true,
+    ];
+    
     private $bytemap;
+    
+    private $element;
 
-    private $lastIndex = 0;
-
-    public function setUp(): void
+    private $lastIndex;
+    
+    public function provideImplementations(): \Generator
     {
+        foreach (self::IMPLEMENTATIONS as $implementation => $element) {
+            $reflectionClass = new \ReflectionClass($implementation);
+            yield $reflectionClass->getShortName() => [$implementation, $element];
+        }
+    }
+
+    public function setUp(array $params): void
+    {
+        [$implementation, $this->element] = $params;
+        $this->bytemap = new $implementation("\x0");
+        $this->lastIndex = 0;
         \mt_srand(0);
-        $this->bytemap = new Bytemap("\x0");
     }
 
     /**
-     * @revs(100000)
+     * @ParamProviders({"provideImplementations"})
+     * @revs(20000)
      */
     public function benchNativeExpand(): void
     {
         $this->lastIndex += \mt_rand(1, 100);
-        $this->bytemap[$this->lastIndex] = "\x1";
+        $this->bytemap[$this->lastIndex] = $this->element;
     }
 }
