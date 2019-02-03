9c85b549889b877f787becfacf0da60c29457795
Robert Gust-Bardon <id+gpg@robert.gust-bardon.org>
2019-02-03 16:31:37 -0500

    Benchmark native implementations, not wrappers

--- tests/Bytemap/Performance/AbstractTestOfPerformance.php
+++ tests/Bytemap/Performance/AbstractTestOfPerformance.php
@@ -13,59 +13,23 @@ declare(strict_types=1);
 
 namespace Bytemap\Performance;
 
-use Bytemap\Benchmark\ArrayBytemap;
-use Bytemap\Benchmark\DsDequeBytemap;
-use Bytemap\Benchmark\DsVectorBytemap;
-use Bytemap\Benchmark\SplBytemap;
-use Bytemap\Bytemap;
-
 /**
  * @author Robert Gust-Bardon <robert@gust-bardon.org>
  *
  * @BeforeMethods({"setUp"})
- * @AfterMethods({"tearDown"})
  *
  * @internal
  */
 abstract class AbstractTestOfPerformance
 {
+    protected const DEFAULT_INSERTED_PAIRS = [
+        [self::DEFAULT_ELEMENT_SHORT, self::INSERTED_ELEMENT_SHORT],
+        [self::DEFAULT_ELEMENT_LONG, self::INSERTED_ELEMENT_LONG],
+    ];
+
     private const DEFAULT_ELEMENT_SHORT = "\x0";
     private const DEFAULT_ELEMENT_LONG = "\x0\x0\x0\x0";
 
     private const INSERTED_ELEMENT_SHORT = "\x1";
     private const INSERTED_ELEMENT_LONG = "\x1\x2\x3\x4";
-
-    private const DEFAULT_INSERTED_PAIRS = [
-        [self::DEFAULT_ELEMENT_SHORT, self::INSERTED_ELEMENT_SHORT],
-        [self::DEFAULT_ELEMENT_LONG, self::INSERTED_ELEMENT_LONG],
-    ];
-
-    private const IMPLEMENTATIONS = [
-        ArrayBytemap::class => self::DEFAULT_INSERTED_PAIRS,
-        DsDequeBytemap::class => self::DEFAULT_INSERTED_PAIRS,
-        DsVectorBytemap::class => self::DEFAULT_INSERTED_PAIRS,
-        SplBytemap::class => self::DEFAULT_INSERTED_PAIRS,
-        Bytemap::class => self::DEFAULT_INSERTED_PAIRS,
-    ];
-
-    protected /* BytemapInterface */ $bytemap;
-    
-    public function provideImplementations(): \Generator
-    {
-        foreach (self::IMPLEMENTATIONS as $implementation => $default_inserted_pairs) {
-            $shortName = (new \ReflectionClass($implementation))->getShortName();
-            $shortName = \preg_replace('~^(.+)Bytemap$~', '${1}', $shortName);
-            foreach ($default_inserted_pairs as [$default, $inserted]) {
-                $testId = $shortName.'-'.\strlen($default);
-                yield $testId => [$testId, $implementation, $default, $inserted];
-            }
-        }
-    }
-    
-    public function setUp(array $params): void
-    {
-        [, $implementation, $defaultElement, , ] = $params;
-        $this->bytemap = new $implementation($defaultElement);
-        \mt_srand(0);
-    }
 }
---
+++ tests/Bytemap/Performance/ExpandPerformance.php
@@ -0,0 +1,145 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Bytemap package.
+ *
+ * (c) Robert Gust-Bardon <robert@gust-bardon.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bytemap\Performance;
+
+use Bytemap\Bytemap;
+use Bytemap\BytemapInterface;
+
+/**
+ * @author Robert Gust-Bardon <robert@gust-bardon.org>
+ *
+ * @internal
+ */
+final class ExpandPerformance extends AbstractTestOfPerformance
+{
+    private const INTERVALS = [1, 10, 100];
+
+    private /* int */ $lastIndex;
+
+    private /* array */ $array;
+
+    private /* BytemapInterface */ $bytemap;
+
+    private /* \Ds\Sequence */ $dsDeque;
+
+    private /* \Ds\Vector */ $dsVector;
+
+    private /* \SplFixedArray */ $splFixedArray;
+
+    public function setUp(array $params): void
+    {
+        [$default, , ] = $params;
+
+        $this->lastIndex = 0;
+
+        $this->array = [];
+        $this->bytemap = new Bytemap($default);
+        $this->dsDeque = new \Ds\Deque([$default]);
+        $this->dsVector = new \Ds\Vector([$default]);
+        $this->splFixedArray = new \SplFixedArray();
+    }
+
+    public function providePairsAndIntervals(): \Generator
+    {
+        foreach (self::DEFAULT_INSERTED_PAIRS as [$default, $inserted]) {
+            foreach (self::INTERVALS as $interval) {
+                yield \sprintf('elementLength:%d maximumGap:%d', \strlen($default), $interval) => [$default, $inserted, $interval];
+            }
+        }
+    }
+
+    /**
+     * @Groups({"Memory"})
+     * @ParamProviders({"providePairsAndIntervals"})
+     * @Revs(100000)
+     */
+    public function benchExpandBaseline(array $params): void
+    {
+    }
+
+    /**
+     * @Groups({"Memory"})
+     * @ParamProviders({"providePairsAndIntervals"})
+     * @Revs(100000)
+     */
+    public function benchExpandWithArray(array $params): void
+    {
+        [, $inserted, $interval] = $params;
+        $this->array[$this->lastIndex += $interval] = $inserted;
+    }
+
+    /**
+     * @Groups({"Memory"})
+     * @ParamProviders({"providePairsAndIntervals"})
+     * @Revs(100000)
+     */
+    public function benchExpandWithBytemap(array $params): void
+    {
+        [, $inserted, $interval] = $params;
+        $this->bytemap[$this->lastIndex += $interval] = $inserted;
+    }
+
+    /**
+     * @Groups({"Memory"})
+     * @ParamProviders({"providePairsAndIntervals"})
+     * @Revs(100000)
+     */
+    public function benchExpandWithDsDeque(array $params): void
+    {
+        [$default, $inserted, $interval] = $params;
+        if ($interval > 1) {
+            $count = $this->lastIndex;
+            $this->lastIndex += $interval;
+            $this->dsDeque->allocate($this->lastIndex);
+            $elements = \array_fill(0, (int) $this->lastIndex - $count - 1, $default);
+            $elements[] = $inserted;
+            $this->dsDeque->push(...$elements);
+        } else {
+            $this->dsDeque->push($inserted);
+        }
+    }
+
+    /**
+     * @Groups({"Memory"})
+     * @ParamProviders({"providePairsAndIntervals"})
+     * @Revs(100000)
+     */
+    public function benchExpandWithDsVector(array $params): void
+    {
+        [$default, $inserted, $interval] = $params;
+        if ($interval > 1) {
+            $count = $this->lastIndex;
+            $this->lastIndex += $interval;
+            $this->dsVector->allocate($this->lastIndex);
+            $elements = \array_fill(0, (int) $this->lastIndex - $count - 1, $default);
+            $elements[] = $inserted;
+            $this->dsVector->push(...$elements);
+        } else {
+            $this->dsVector->push($inserted);
+        }
+    }
+
+    /**
+     * @Groups({"Memory"})
+     * @ParamProviders({"providePairsAndIntervals"})
+     * @Revs(100000)
+     */
+    public function benchExpandWithSplFixedArray(array $params): void
+    {
+        [, $inserted, $interval] = $params;
+        $this->lastIndex += $interval;
+        $this->splFixedArray->setSize($this->lastIndex + 1);
+        $this->splFixedArray[$this->lastIndex] = $inserted;
+    }
+}
