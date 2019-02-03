dc4f17cc23ca94605135d5081e862f0163cc5c41
Robert Gust-Bardon <id+gpg@robert.gust-bardon.org>
2019-02-02 23:56:40 -0500

    Introduce the `AbstractTestOfPerformance` class

--- tests/Bytemap/Performance/AbstractTestOfPerformance.php
+++ tests/Bytemap/Performance/AbstractTestOfPerformance.php
@@ -13,12 +13,11 @@ declare(strict_types=1);
 
 namespace Bytemap\Performance;
 
-use Bytemap\Bitmap;
-use Bytemap\Bytemap;
 use Bytemap\Benchmark\ArrayBytemap;
 use Bytemap\Benchmark\DsDequeBytemap;
 use Bytemap\Benchmark\DsVectorBytemap;
 use Bytemap\Benchmark\SplBytemap;
+use Bytemap\Bytemap;
 
 /**
  * @author Robert Gust-Bardon <robert@gust-bardon.org>
@@ -28,19 +27,19 @@ use Bytemap\Benchmark\SplBytemap;
  *
  * @internal
  */
-final class BytemapPerformance
+abstract class AbstractTestOfPerformance
 {
     private const DEFAULT_ELEMENT_SHORT = "\x0";
     private const DEFAULT_ELEMENT_LONG = "\x0\x0\x0\x0";
-    
+
     private const INSERTED_ELEMENT_SHORT = "\x1";
     private const INSERTED_ELEMENT_LONG = "\x1\x2\x3\x4";
-    
+
     private const DEFAULT_INSERTED_PAIRS = [
         [self::DEFAULT_ELEMENT_SHORT, self::INSERTED_ELEMENT_SHORT],
         [self::DEFAULT_ELEMENT_LONG, self::INSERTED_ELEMENT_LONG],
     ];
-    
+
     private const IMPLEMENTATIONS = [
         ArrayBytemap::class => self::DEFAULT_INSERTED_PAIRS,
         DsDequeBytemap::class => self::DEFAULT_INSERTED_PAIRS,
@@ -48,53 +47,25 @@ final class BytemapPerformance
         SplBytemap::class => self::DEFAULT_INSERTED_PAIRS,
         Bytemap::class => self::DEFAULT_INSERTED_PAIRS,
     ];
-    
-    private $bytemap;
-    
-    private $element;
 
-    private $lastIndex;
-    
-    private $operationCount;
+    protected /* BytemapInterface */ $bytemap;
     
     public function provideImplementations(): \Generator
     {
         foreach (self::IMPLEMENTATIONS as $implementation => $default_inserted_pairs) {
             $shortName = (new \ReflectionClass($implementation))->getShortName();
+            $shortName = \preg_replace('~^(.+)Bytemap$~', '${1}', $shortName);
             foreach ($default_inserted_pairs as [$default, $inserted]) {
-                $key = $shortName.'-'.\strlen($default);
-                yield $key => [$implementation, $default, $inserted];
+                $testId = $shortName.'-'.\strlen($default);
+                yield $testId => [$testId, $implementation, $default, $inserted];
             }
         }
     }
-
+    
     public function setUp(array $params): void
     {
-        [$implementation, $defaultElement, $this->element] = $params;
+        [, $implementation, $defaultElement, , ] = $params;
         $this->bytemap = new $implementation($defaultElement);
-        $this->lastIndex = 0;
-        $this->operationCount = 0;
         \mt_srand(0);
-    }
-   
-    public function tearDown(array $params): void
-    {
-        [, , $insertedElement] = $params;
-        $operationCount = 0;
-        foreach ($this->bytemap->find([$insertedElement]) as $element) {
-            ++$operationCount;
-        }
-        \assert($this->operationCount === $operationCount);
-    }
-
-    /**
-     * @ParamProviders({"provideImplementations"})
-     * @Revs(20000)
-     */
-    public function benchNativeExpand(): void
-    {
-        $this->lastIndex += \mt_rand(1, 100);
-        $this->bytemap[$this->lastIndex] = $this->element;
-        ++$this->operationCount;
     }
 }
